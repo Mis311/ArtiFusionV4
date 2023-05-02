fc905324ede2d5a15cfba1ca3296164ce428646b
Mis311 <ley.hanamura@gmail.com>
2023-05-03 01:27:41 +0900

    data.js  add

---
+++ src/frontend/pages/user/login/data.js
@@ -0,0 +1,10 @@
+export const dataSet = [
+    {
+      background: '/path/to/first/background.png',
+      character1: '/path/to/first/character1.png',
+      character2: '/path/to/first/character2.png',
+      text: 'First set of text',
+    },
+    // ... add more sets
+  ];
+  
